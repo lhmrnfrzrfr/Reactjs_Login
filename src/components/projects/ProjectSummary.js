9598e250bd7267ba72f40509705d134ca6018cd1
unknown <ilhamriana3@gmail.com>
2019-03-07 03:43:07 +0700

    update-project

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-tittle">Project Title</span>
+        <p>Posted by lhmrnfrzrfr</p>
+        <p className="grey-text">6 March, 3:40am</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummary;
